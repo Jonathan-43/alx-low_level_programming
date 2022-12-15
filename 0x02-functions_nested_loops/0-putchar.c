a5350d1c90cbd7359848098ade3d4ff8c00a8bfb
jonathan-43 <yonasw2011@gamil.com>
2022-12-16 00:37:07 +0300

    correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include <main.h>
+#include "main.h"
 /**
- * main - prints _putchar
- * Return: to 0
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
-	char yo[] = "_putchar";
+	char a[] = "_putchar";
 
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(yo[c]);
+		_putchar(a[c]);
 	}
 	_putchar('\n');
 	return (0);
